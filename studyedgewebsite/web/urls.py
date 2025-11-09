0a9d105120ca2f772e2f390049473219981e1b7d
sixdgt <c4crypt@gmail.com>
2025-11-09 10:18:23 +0545

    fix: destination, ielts/pte & update: destination detail and abroad study

--- studyedgewebsite/web/urls.py
+++ studyedgewebsite/web/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from web.views import index
+from web.views import destination, index, study_abroad_step
 
 urlpatterns = [
     path('', index, name='index'),
+    path('destinations/australia/', destination, name='australia'),
+    path('study-abroad-steps/', study_abroad_step, name='study_abroad_steps'),
 ]
--- studyedgewebsite/web/views.py
+++ studyedgewebsite/web/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    student_support_title = "Study Edge help you in every step."
+    return render(request, 'index.html', {'student_support_title': student_support_title})
+
+def destination(request):
+    student_support_title = "Find your perfect study destination with StudyEdge"
+    return render(request, 'destinations/destination_detail.html', {'student_support_title': student_support_title})
+
+def study_abroad_step(request):
+    student_support_title = "Applying made easy with StudyEdge"
+    return render(request, 'study_abroad_steps.html', {'student_support_title': student_support_title})
