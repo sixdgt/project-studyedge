71ca578adcd1cd14d0acddd2ac29997820a2be3e
sixdgt <c4crypt@gmail.com>
2025-12-03 17:55:07 +0545

    fix: header and footer

--- studyedgewebsite/studyedgewebsite/settings.py
+++ studyedgewebsite/studyedgewebsite/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
-NPM_BIN_PATH = 'C:/Program Files/nodejs/npm.cmd'
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
