e9a59df9d752d4f4e4003c46f435725d14219c44
sixdgt <c4crypt@gmail.com>
2025-12-16 16:53:04 +0545

    theme: removed for test

--- studyedgewebsite/settings.py
+++ studyedgewebsite/settings.py
@@ -40,11 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tailwind',
-    'theme',
     'web',
 ]
-
-TAILWIND_APP_NAME = 'theme'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
