42c07b0228b57e43cc42f59208d69fcab1e0cb36
sixdgt <c4crypt@gmail.com>
2025-12-20 16:14:55 +0545

    udpate: settings.py

--- studyedgewebsite/settings.py
+++ studyedgewebsite/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-# import environ
+import environ
 
-# env = environ.Env()
-# environ.Env.read_env(".env")
+env = environ.Env()
+environ.Env.read_env(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&mgq&$1q=d4#v-($ss$2)8^+6=w4rj3foes*^scsd7e-c$wc4%"
+SECRET_KEY = env.str("SECRET_KEY", "django-insecure-&mgq&$1q=d4#v-($ss$2)8^+6=w4rj3foes*^scsd7e-c$wc4%")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
 # Application definition
 
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'web',
 ]
 
-TAILWIND_APP_NAME = "theme"
+TAILWIND_APP_NAME = 'theme'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -81,14 +81,7 @@ WSGI_APPLICATION = 'studyedgewebsite.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'studyedge',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': env.db()
 }
 
 
@@ -126,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
